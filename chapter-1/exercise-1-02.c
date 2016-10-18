bdf6496bb734cebbada17bd410f3905f666e7539
Victor Samson <victor.samson@gmail.com>
2016-10-18 13:34:38 +1100

    Added some Chapter 1 solutions.

---
+++ chapter-1/exercise-1-02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Experimenting with unknown escape sequence \q");
+}
---
+++ chapter-1/exercise-1-04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20, ..., 300; floating-point version  */
+main()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature scale */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    printf("%3s %6s\n", "Cel", "Fah");
+    printf("==========\n");
+
+    fahr = lower;
+    while (celsius <= upper) {
+        fahr = celsius * (9.0/5.0) + 32.0;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
